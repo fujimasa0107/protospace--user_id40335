eda767108362837fd99e602db7958759b09cf098
fujimasa0107 <158474627+fujimasa0107@users.noreply.github.com>
2024-02-17 21:47:34 +0900

    imgストロングパラメーター記入

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,4 +3,11 @@ class PrototypesController < ApplicationController
     @user_name = current_user.name if user_signed_in?
   end
 
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,3 +1,4 @@
 class Prototype < ApplicationRecord
   has_many :user
+  has_one_attached :image
 end
