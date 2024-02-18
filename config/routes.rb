72cae07e71217d4b7beb44dcb37d5a637270003f
fujimasa0107 <158474627+fujimasa0107@users.noreply.github.com>
2024-02-19 08:59:01 +0900

    コントローラー作成、ルーティング指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "prototypes#index"
-  resources :prototypes
+  root to: 'prototypes#index'
+  resources :prototypes do
+    resources :comments, only: :create
+  end
 end
